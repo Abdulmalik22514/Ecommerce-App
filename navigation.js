c060a19d6fad473ace747c45216a4515e8aa9146
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-01 17:33:24 +0100

    chore: change file path

--- navigation.js
+++ navigation.js
@@ -1,14 +1,14 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createStackNavigator } from "@react-navigation/stack";
-import Account from "./screens/secondPage";
-import FirstPage from "./screens/firstPage";
-import Home from "./screens/thirdPage";
-import SelectPage from "./screens/fourthPage";
-import Preview from "./screens/fifthPage";
-import PaymentStatus from "./screens/sixthPage";
-import CustomDrawer from "./screens/drawer";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import Account from './screens/CreateAccount/createAcct';
+import Home from './screens/thirdPage';
+import SelectPage from './screens/fourthPage';
+import Preview from './screens/fifthPage';
+import PaymentStatus from './screens/sixthPage';
+import CustomDrawer from './screens/drawer';
+import Onboarding from './screens/Onboarding/onboarding';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -16,7 +16,7 @@ const Drawer = createDrawerNavigator();
 function StackNav() {
   return (
     <Stack.Navigator headerMode="none">
-      <Stack.Screen name="first" component={FirstPage} />
+      <Stack.Screen name="onboarding" component={Onboarding} />
       <Stack.Screen name="account" component={Account} />
       <Stack.Screen name="home" component={Home} />
       <Stack.Screen name="select" component={SelectPage} />
@@ -28,7 +28,7 @@ function StackNav() {
 
 const Navigator = () => (
   <NavigationContainer>
-    <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props} />}>
+    <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />}>
       <Drawer.Screen name="home" component={StackNav} />
       {/* <Drawer.Screen name="select" component={StackNav} /> */}
     </Drawer.Navigator>
